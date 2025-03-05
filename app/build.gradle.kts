ddcc26211ff5ea6ab924ea5f3ff69eb7db4f0f01
Shyamkumar <shyamkumar@live.com>
2025-03-05 15:50:15 +0530

    startup build failures fixed and required dependencies added
    Signed-off-by: shyamkumarm-ctct <shyamkumar_mathivanan@trimble.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+
 }
 
 android {
     namespace = "com.gallery.myapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.gallery.myapplication"
