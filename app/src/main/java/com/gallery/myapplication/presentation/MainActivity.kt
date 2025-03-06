3fc0edf1fe031414a5e2825de2c03b73912bfebf
shyamkumarm-ctct <shyamkumar_mathivanan@trimble.com>
2025-03-06 21:46:36 +0530

    Compose Ui view changed, Material theme declared

--- app/src/main/java/com/gallery/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/gallery/myapplication/presentation/MainActivity.kt
@@ -13,12 +13,10 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -26,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import coil3.compose.setSingletonImageLoaderFactory
 import com.gallery.myapplication.R
 import com.gallery.myapplication.presentation.screen.MyNavigationScreen
+import com.gallery.myapplication.ui.theme.MyApplicationTheme
 import com.gallery.myapplication.utils.CoilImageLoader
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -41,7 +40,7 @@ class MainActivity : ComponentActivity() {
                         setSingletonImageLoaderFactory { context ->
                             CoilImageLoader.getAsyncImageLoader(context)
                         }
-                        MaterialTheme {
+                        MyApplicationTheme() {
 
                             Scaffold(topBar = { TopAppBarDefaults() }) { innerPadding ->
                                 Surface(
@@ -72,10 +71,6 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun TopAppBarDefaults() {
         TopAppBar(
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                titleContentColor = MaterialTheme.colorScheme.primary,
-            ),
             title = {
                 Text(text = stringResource(R.string.app_name), maxLines = 1)
             },
--- app/src/main/java/com/gallery/myapplication/presentation/screen/ItemScreen.kt
+++ app/src/main/java/com/gallery/myapplication/presentation/screen/ItemScreen.kt
@@ -4,17 +4,13 @@ import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -24,15 +20,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
-import coil3.size.Size
 import com.gallery.myapplication.R
 import com.gallery.myapplication.domain.GalleryItem
 import com.gallery.myapplication.domain.MediaItem
-import com.gallery.myapplication.utils.CoilImageLoader
 
 
 @Composable
@@ -40,30 +35,35 @@ fun FolderListScreen(
     fileItems: List<GalleryItem>,
     onFolderClick: (String) -> Unit
 ) {
-    LazyVerticalGrid(
-        modifier = Modifier,
-        columns = GridCells.Fixed(4)
-    ) {
-        items(items = fileItems, key = { it.folderId }) { it ->
-            FolderItem(it.folderName, it.fileList) {
-                onFolderClick(it.folderId)
+   /* Column {
+        Text(
+           " fileItems[0].folderName",
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.titleLarge
+        )*/
+        LazyVerticalGrid(columns = GridCells.Fixed(3), modifier = Modifier.padding(8.dp)) {
+            items(items = fileItems, key = { it.folderId }) { it ->
+                FolderItem(it.folderName, it.fileList) {
+                    onFolderClick(it.folderId)
+                }
             }
         }
-    }
+   // }
 }
 
 @Composable
 fun FileListScreen(fileItems: List<MediaItem>) {
-    LazyVerticalStaggeredGrid(
-        modifier = Modifier,
-        contentPadding = PaddingValues(10.dp),
-        columns = StaggeredGridCells.Fixed(3),
-        verticalItemSpacing = 5.dp,
-        horizontalArrangement = Arrangement.SpaceAround
-    ){
-        items(items = fileItems) {
-            FileItem(it) {
-                // event required when item clicked
+    Column {
+        Text(
+            fileItems[0].folderName,
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.titleLarge
+        )
+        LazyVerticalGrid(columns = GridCells.Fixed(3), modifier = Modifier.padding(8.dp)) {
+            items(items = fileItems) {
+                FileItem(it) {
+                    // event required when item clicked
+                }
             }
         }
     }
@@ -76,175 +76,119 @@ fun FolderItem(
     onMediaItemClick: () -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Card(
             modifier = Modifier
-                .clickable { onMediaItemClick() },
+                .padding(8.dp)
+                .clickable { onMediaItemClick() }
+                .aspectRatio(1f),
             shape = RoundedCornerShape(8.dp)
         ) {
             AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(fileList[0].uri)
-                    .size(Size(200,200)) // Set the target size to load the image at.
-                    .build(),
+                model = fileList[0].uri,
                 contentDescription = "Media",
-                   imageLoader = CoilImageLoader.getAsyncImageLoader(LocalContext.current),
                 contentScale = ContentScale.Crop
             )
         }
-        Row {
-            Text(
-                text = folderName,
-                style = MaterialTheme.typography.titleSmall, modifier = Modifier.padding(4.dp)
-            )
-            Text(
-                text = fileList.size.toString(),
-                style = MaterialTheme.typography.titleSmall, modifier = Modifier.padding(4.dp)
-            )
-        }
+        val mTextModifier = Modifier
+            .align(Alignment.Start)
+            .padding(start = 8.dp)
+        Text(
+            text = folderName,
+            modifier = mTextModifier,
+            maxLines = 1,
+            fontSize = 12.sp,
+            overflow = TextOverflow.Ellipsis
+        )
+        Spacer(modifier = Modifier.height(1.dp))
+        Text(fileList.size.toString(), modifier = mTextModifier, fontSize = 10.sp)
+
     }
 }
 
-    @Composable
-    fun FileItem(
-        mediaItem: MediaItem,
-        onMediaItemClick: () -> Unit
+@Composable
+fun FileItem(
+    mediaItem: MediaItem,
+    onMediaItemClick: () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Card(
+            modifier = Modifier
+                .padding(8.dp)
+                .clickable { onMediaItemClick() }
+                .aspectRatio(1f),
+            shape = RoundedCornerShape(8.dp)
         ) {
-            Card(
-                modifier = Modifier
-                    .padding(4.dp)
-                    .clickable { onMediaItemClick() },
-                shape = RoundedCornerShape(8.dp)
-            ) {
-                AsyncImage(
-                    model = mediaItem.uri,
-                    contentDescription = "Media",
-                    contentScale = ContentScale.Crop
-                )
-            }
-            Row {
-                Text(
-                    text =  mediaItem.fileName,
-                    style = MaterialTheme.typography.titleSmall, modifier = Modifier.padding(4.dp)
-                )
-            }
+            AsyncImage(
+                model = mediaItem.uri,
+                contentDescription = "Media",
+                contentScale = ContentScale.Crop
+            )
         }
+        val mTextModifier =
+            Text(
+                mediaItem.fileName,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(start = 8.dp),
+                fontSize = 12.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
     }
+}
 
 
-
-    @Preview(showSystemUi = true, showBackground = true)
-    @Composable
-    fun FolderListPreview(
-        fileItems: List<GalleryItem> = listOf(
-            GalleryItem(
-                folderId = "1", folderName = "All Images",
-                fileList = listOf(
-                    MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    )
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun FolderListPreview(
+    fileItems: List<GalleryItem> = listOf(
+        GalleryItem(
+            folderId = "1", folderName = "All Images",
+            fileList = listOf(
+                MediaItem(
+                    uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
+                    fileName = "IMG_20210509_164609", folderName = "Camera"
                 )
-            ),
+            )
+        ),
 
-            GalleryItem(
-                folderId = "2", folderName = "All Images",
-                fileList = listOf(
-                    MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    )
+        GalleryItem(
+            folderId = "2", folderName = "All Images",
+            fileList = listOf(
+                MediaItem(
+                    uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
+                    fileName = "IMG_20210509_164609", folderName = "Camera"
                 )
-            ),
+            )
+        ),
 
-            GalleryItem(
-                folderId = "3", folderName = "All Images",
-                fileList = listOf(
-                    MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    )
+        GalleryItem(
+            folderId = "3", folderName = "All Images",
+            fileList = listOf(
+                MediaItem(
+                    uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
+                    fileName = "IMG_20210509_164609", folderName = "Camera"
                 )
-            ),
+            )
+        ),
 
-            GalleryItem(
-                folderId = "4", folderName = "All Images",
-                fileList = listOf(
-                    MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_background),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    ), MediaItem(
-                        uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
-                        fileName = "IMG_20210509_164609", folderName = "Camera"
-                    )
+        GalleryItem(
+            folderId = "4", folderName = "All Images",
+            fileList = listOf(
+                MediaItem(
+                    uri = Uri.parse("android.resource://" + LocalContext.current.packageName + "/" + R.drawable.ic_launcher_foreground),
+                    fileName = "IMG_20210509_164609", folderName = "Camera"
                 )
             )
         )
-    ) {
-        FolderListScreen(fileItems) {
-
-        }
+    )
+) {
+    FolderListScreen(fileItems) {
     }
+}
